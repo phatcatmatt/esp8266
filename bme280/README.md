0ba6ffcdff6563f38c0f90bb409deb91dee6701a
Matt Nicholas <matt@mattnicholas.io>
2020-03-24 13:10:31 -0400

    add readme and second library

---
+++ bme280/README.md
@@ -0,0 +1,13 @@
+There are two libraries you can use.
+
+### Option 1 - Robert HH
+
+[Library](https://github.com/robert-hh/BME280)
+
+### Option 2 - Rui Stantos
+
+[Tutorial](https://randomnerdtutorials.com/low-power-weather-station-datalogger-using-esp8266-bme280-micropython/)
+
+[Code](https://github.com/RuiSantosdotme/ESP-MicroPython/tree/master/code/WiFi/HTTP_Client_IFTTT_BME280)
+
+[Library](https://raw.githubusercontent.com/RuiSantosdotme/ESP-MicroPython/master/code/WiFi/HTTP_Client_IFTTT_BME280/BME280.py)
--- bme280/main.py
+++ bme280/main.py
@@ -1,7 +1,19 @@
 from machine import Pin, I2C
-import bme280_float as bme280
+# import bme280_float as bme280
+import BME280
 
 i2c = I2C(scl=Pin(5), sda=Pin(4))
-bme = bme280.BME280(i2c=i2c)
+# bme = bme280.BME280(i2c=i2c)
+# print(bme.values)
 
-print(bme.values)
+bme = BME280.BME280(i2c=i2c)
+temp = bme.temperature
+hum = bme.humidity
+pres = bme.pressure
+
+# uncomment for temperature in Fahrenheit
+temp = (bme.read_temperature()/100) * (9/5) + 32
+temp = str(round(temp, 2)) + 'F'
+
+sensor_readings = {'value1': temp, 'value2': hum, 'value3': pres}
+print(sensor_readings)
